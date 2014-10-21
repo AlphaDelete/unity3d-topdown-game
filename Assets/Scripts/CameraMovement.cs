68c69378532ed2492037462ee5fa84ca932bec81
RafaelDelboni <rafadelboni@gmail.com>
2014-10-21 02:07:24 -0200

    First Commit

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMovement : MonoBehaviour {
+
+	public Transform target;
+	public float distanceDamping = 3.0F;
+	public float heightDamping = 1.0F;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	void LateUpdate () {
+		if (!target)
+			return;
+		
+		float wantedHeight = target.position.y;
+		float wantedDistance = target.position.x;
+
+		float currentHeight = transform.position.y;
+		float currentDistance = transform.position.x;
+		
+		currentHeight = Mathf.Lerp (currentHeight, wantedHeight, heightDamping * Time.deltaTime);
+		currentDistance = Mathf.Lerp (currentDistance, wantedDistance, distanceDamping * Time.deltaTime);
+		
+		this.transform.position = new Vector3 (currentDistance, currentHeight, -10f);
+	}
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public float Speed;
+	public Animator Anim;
+	private Vector3 Movement;
+
+	void Update()
+	{
+		// Set Player movement
+		SetMovement();
+	}
+
+	#region SetMovement
+	void SetMovement() {
+		float moveHorizontal = Input.GetAxisRaw("Horizontal");
+		float moveVertical = Input.GetAxisRaw("Vertical");
+		
+		Movement = new Vector2(moveHorizontal, moveVertical);
+		Movement.Normalize();
+		rigidbody.velocity = Movement * Speed;
+	}
+	#endregion
+
+	#region setAnimation
+	void setAnimation(int animNumber) 
+	{
+		if(animNumber == Anim.GetInteger("animation") || animNumber == -Anim.GetInteger("animation"))
+		{
+			Anim.SetInteger("animation", animNumber);
+		} else {
+			Anim.SetInteger("animation", -animNumber);
+		}
+	}
+	#endregion
+}
