5556d80cd2d1b8ccd7ac96706458edd4135d8e38
RafaelDelboni <rafadelboni@gmail.com>
2014-10-29 01:02:10 -0200

    Add/Modded Pickup Item and Drop - Part 2

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -10,43 +10,52 @@ public class PlayerControl : MonoBehaviour {
 	private float timerAtk = 0.4f;
 	private float attackTimer ;
 	private int attacking = 0;
+	private bool picking = false;
+	private Transform pickedItem = null;
+
+	private bool walking = false;
+	private float playerLookX;
+	private float playerLookY; 
 
 	// Access camera to shake
 	public GameObject playerCamera;	
 	private CameraMovement moveCamera;
 
+
 	void Start() {
 		moveCamera = playerCamera.GetComponent<CameraMovement>();
 	}
 
+	#region OnFrameUpdate
+	// Update each frame
 	void Update() {
 		setAttack();
+		setGrabThrow();
 	}
-
+	// Update each frame * deltaTime
 	void FixedUpdate()
 	{
 		// Set Player movement
 		setMovement();
 	}
+	#endregion
 
-	void OnTriggerEnter(Collider other) {
-		//Destroy(other.gameObject);
+	#region TriggerEvents
+	// Check colisions each frame
+	void OnTriggerEnter2D(Collider2D other) {
+		Debug.Log("E");
 		Transform parentCollider = other.gameObject.transform.parent;
-		if (parentCollider.tag == "Pickup") 
+		if (parentCollider.tag == "Pickup" && picking == false && pickedItem == null) 
 		{
-			Debug.Log(parentCollider.tag);
-			parentCollider.GetComponent<SpriteRenderer>().sortingLayerName = "Pickup";
-			parentCollider.transform.parent = this.transform; 
-			parentCollider.transform.localPosition = new Vector3(0, 0.6f, 0);
+			pickedItem = parentCollider;
 		}
-
-		/*
-		 	//trow
-			parentCollider.transform.parent = null;
-			parentCollider.rigidbody.AddForce (transform.forward * 8000);
-		*/
-
 	}
+	void OnTriggerExit2D() {
+		if (picking == false) {
+			pickedItem = null;
+		}
+	}
+	#endregion
 
 	#region Movement
 	void setMovement() {
@@ -55,20 +64,25 @@ public class PlayerControl : MonoBehaviour {
 
 		if ( moveHorizontal == 0 && moveVertical == 0) {
 			Anim.SetBool("walking", false);
+			walking = false;
 		} else {
 			Anim.SetBool("walking", true);
 			Anim.SetFloat("xAxis", moveHorizontal);
 			Anim.SetFloat("yAxis", moveVertical);
+			// Save Last movement
+			playerLookX = moveHorizontal;
+			playerLookY = moveVertical;
+			walking = true;
 		}
 
 		Movement = new Vector2(moveHorizontal, moveVertical);
-		rigidbody.velocity = Movement.normalized * Speed;
+		rigidbody2D.velocity = Movement.normalized * Speed;
 	}
 	#endregion
 
-	#region Attack
+	#region Attack/Damage
 	void setAttack() {
-		if (Input.GetButtonDown ("Fire1") && attackTimer == 0) {
+		if (Input.GetButtonDown ("Fire1") && attackTimer == 0 && picking == false) {
 			// Set the attack anim
 			attacking = 1;
 			attackTimer = timerAtk;
@@ -87,5 +101,40 @@ public class PlayerControl : MonoBehaviour {
 		Anim.SetInteger("attacking", attacking);
 	}
 	#endregion
-}
 
+	#region Grab/Throw
+	void setGrabThrow() {
+		if (pickedItem != null ) {
+			if (Input.GetButtonDown ("Fire2") && attacking == 0 && picking == false) {
+				setGrab();
+			} else if (Input.GetButtonDown ("Fire2") && picking == true ) {
+				setThrow();
+			}
+		}
+		Anim.SetBool("picking", picking);
+	}
+
+	void setGrab() {
+		picking = true;
+		pickedItem.GetComponent<SpriteRenderer>().sortingLayerName = "Pickup";
+		pickedItem.rigidbody2D.isKinematic = true;
+		pickedItem.transform.parent = this.transform; 
+		pickedItem.transform.localPosition = new Vector3(0, 0.6f, 0);
+	}
+
+	void setThrow() {
+		if (!walking) {
+			pickedItem.transform.parent = null;
+			pickedItem.rigidbody2D.isKinematic = true;
+			pickedItem.rigidbody2D.position = 
+					new Vector2(
+						transform.position.x + pickedItem.collider2D.bounds.size.x * playerLookX, 
+						transform.position.y + pickedItem.collider2D.bounds.size.y * playerLookY
+					);
+			pickedItem.GetComponent<SpriteRenderer>().sortingLayerName = "Objects";
+			picking = false;
+		}
+
+	}
+	#endregion
+}
